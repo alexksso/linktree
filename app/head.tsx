06ac3f3d73f30a867780b9a0ae249a9605e32a6c
Alex Casassovici <alex@ksso.net>
2023-10-21 16:24:56 -0700

    change favicon

--- app/head.tsx
+++ app/head.tsx
@@ -3,7 +3,7 @@ export default function Head() {
     <>
       <title>Linktree</title>
       <meta name="description" content="Next.js + Tailwind CSS" />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/favicon.png" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
     </>
   );
