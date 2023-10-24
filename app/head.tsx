986c13bf94f4c543f1eb9e4371f50f7399837f9c
Alex Casassovici <alex@ksso.net>
2023-10-24 13:39:20 -0700

    forgot to remove a comment

--- app/head.tsx
+++ app/head.tsx
@@ -5,7 +5,6 @@ export default function Head() {
       <meta name="description" content="Alex's Links" />
       <link rel="icon" href="/favicon.png" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      // add a link to my image for previews
       <meta property="og:image" content="https://links.zatmonkey.com/favicon.png" />
     </>
   );
