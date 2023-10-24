d5e42d79690527457d9d388bb176af2999f5ac79
Alex Casassovici <alex@ksso.net>
2023-10-24 09:58:43 -0700

    updated header

--- app/head.tsx
+++ app/head.tsx
@@ -2,9 +2,11 @@ export default function Head() {
   return (
     <>
       <title>Alex's Links</title>
-      <meta name="description" content="Next.js + Tailwind CSS" />
+      <meta name="description" content="Alex's Links" />
       <link rel="icon" href="/favicon.png" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      // add a link to my image for previews
+      <meta property="og:image" content="https://links.zatmonkey.com/favicon.png" />
     </>
   );
 }
